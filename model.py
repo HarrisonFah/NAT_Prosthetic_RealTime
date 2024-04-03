fixed error in average reward loss and implemeted fft

model.py
@@ -13,26 +13,33 @@ def one_hot(num_classes, class_idx):
 class NeuralNet(nn.Module):
     def __init__(self, num_features):
         super(NeuralNet, self).__init__()
-        self.linear1 = nn.Linear(num_features, num_features, bias=False)
-        torch.nn.init.kaiming_uniform_(self.linear1.weight, nonlinearity="relu")
-        #torch.nn.init.zeros_(self.linear1.bias)
-        self.relu = nn.ReLU()
-        self.linear2 = nn.Linear(num_features, 1, bias=False)
-        torch.nn.init.kaiming_uniform_(self.linear2.weight, nonlinearity="relu")
-        #torch.nn.init.zeros_(self.linear2.bias)
-        self.sig = nn.Sigmoid()
+        self.linear = nn.Linear(num_features, 1, bias=True)
+        torch.nn.init.constant_(self.linear.weight, 0)
+        torch.nn.init.constant_(self.linear.bias, 0)
+        # self.linear1 = nn.Linear(num_features, num_features, bias=True)
+        # torch.nn.init.kaiming_uniform_(self.linear1.weight, nonlinearity="relu")
+        # #torch.nn.init.kaiming_uniform_(self.linear1.bias, nonlinearity="relu")
+        # #torch.nn.init.zeros_(self.linear1.bias)
+        # self.relu = nn.ReLU()
+        # self.linear2 = nn.Linear(num_features, 1, bias=True)
+        # torch.nn.init.kaiming_uniform_(self.linear2.weight, nonlinearity="relu")
+        # #torch.nn.init.kaiming_uniform_(self.linear2.bias, nonlinearity="relu")
+        # #torch.nn.init.zeros_(self.linear2.bias)
+        # self.sig = nn.Sigmoid()
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
         x = x.float()
-        pred = self.linear1(x)
-        pred = self.relu(pred)
-        pred = self.linear2(pred)
-        pred = self.sig(pred)
+        pred = self.linear(x)
+        pred = self.tanh(pred)*2
+        #pred = self.relu(pred)
+        #pred = self.linear2(pred)
+        #pred = self.sig(pred)
 
         return pred
     
 class QLearner():
-    def __init__(self, num_actions, num_features, epsilon = 5e-2, alpha=1e-3, eta=1e-3):
+    def __init__(self, num_actions, num_features, epsilon = 5e-2, alpha=1e-4, eta=1e-1):
         self.num_actions = num_actions
         self.num_features = num_features
         self.network = NeuralNet(num_features+num_actions).to(torch.float)
@@ -46,15 +53,15 @@ class QLearner():
         self.curr_action = None
         self.optimizer = torch.optim.SGD(self.network.parameters(), lr=alpha)
 
-    def loss(self, x, reward, action, x_prime):
+    def loss(self, x, reward, action, x_prime, predicted_reward):
         print("reward:", reward)
         print("self.r_bar:", self.r_bar)
         print("max Q(S', a):", torch.max(torch.tensor([self.network(torch.cat((x_prime, one_hot(self.num_actions, a)))) for a in range(self.num_actions)])))
-        print("Q(S,A):", self.network(torch.cat((x, one_hot(self.num_actions, action)))))
-        return reward \
+        print("Q(S,A):", predicted_reward)
+        return (reward \
             - self.r_bar \
             + torch.max(torch.tensor([self.network(torch.cat((x_prime, one_hot(self.num_actions, a)))) for a in range(self.num_actions)])) \
-            - self.network(torch.cat((x, one_hot(self.num_actions, action))))
+            - predicted_reward)**2
 
     def step(self, x, reward):
         #selects action using epsilon-greedy policy
@@ -78,14 +85,19 @@ class QLearner():
         else:
             selected_action = max_action
         #calculate td error and backpropagate
-        predicted_reward = self.network(torch.cat((x, one_hot(self.num_actions, action))))
+        predicted_reward = 0
         if (self.curr_state != None): 
-            delta = self.loss(self.curr_state, reward, self.curr_action, x) # x here is S'
-            print(delta.detach())
+            predicted_reward = self.network(torch.cat((self.curr_state, one_hot(self.num_actions, action))))
+            delta = self.loss(self.curr_state, reward, self.curr_action, x, predicted_reward) # x here is S'
+            print(delta)
             self.r_bar += self.eta*self.alpha*delta.detach()
             delta.backward()
-            print(self.network.linear1.weight.grad)
-            print(self.network.linear2.weight.grad)
+            print("Linear weight grads")
+            print(self.network.linear.weight.grad)
+            print("Linear bias grads")
+            print(self.network.linear.bias.grad)
+            # print(self.network.linear1.weight.grad)
+            # print(self.network.linear2.weight.grad)
             self.optimizer.step()
         self.curr_state = x # set S
         #self.curr_reward = predicted_reward  # set R

rlplot.py
@@ -26,7 +26,8 @@ class MainRLWindow(QtWidgets.QMainWindow):
         self.num_points = 100
         self.num_samples = num_samples
         self.num_baseline_samples = num_baseline_samples
-        self.learner = QLearner(3, self.num_samples*len(self.eeg_channels))
+        #self.learner = QLearner(3, self.num_samples*len(self.eeg_channels))
+        self.learner = QLearner(2, self.num_samples*len(self.eeg_channels))
         self.queued_reward = 0
 
         # Temperature vs time dynamic plot
@@ -75,7 +76,8 @@ class MainRLWindow(QtWidgets.QMainWindow):
         state_data /= 1000
         eeg_data = state_data[self.eeg_channels]
         flat_eeg_data = torch.flatten(torch.tensor(eeg_data)).double()
-        selected_action, predicted_reward = self.learner.step(flat_eeg_data, self.queued_reward)
+        fft_eeg_data = torch.flatten(torch.fft.fft(flat_eeg_data))
+        selected_action, predicted_reward = self.learner.step(fft_eeg_data, self.queued_reward)
         print("Selected action:", actions[selected_action], ", predicted reward:", predicted_reward[0].item())
 
         self.predictions.pop(0)